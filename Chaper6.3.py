1fcb5024ebd70874d8a22677c560c8d8801384ae
yangshunfan <15700734908@163.com>
2018-01-31 16:31:52 +0800

    创建函数

--- Chaper6.3.py
+++ Chaper6.3.py
@@ -1,19 +1,19 @@
-# # 创建函数
-# def sayHello(name):
-#     return('Hello,' + name + '!')
-# print(sayHello('yangshunfan'))
-#
-# # ---------
-#
-# # 6.3.1 文化档函数
-# # 这里给一个函数增加解释性文档，相当于注释
-# def square(x):
-#     '计算x的平方'
-#     return x*x
-# # 查看方法文档(这里要去掉后面的括号)：方法名._doc_
-# print(square.__doc__)
-# y = square(2)
-# print(y)
+# 创建函数
+def sayHello(name):
+    return('Hello,' + name + '!')
+print(sayHello('yangshunfan'))
+
+# ---------
+
+# 6.3.1 文化档函数
+# 这里给一个函数增加解释性文档，相当于注释
+def square(x):
+    '计算x的平方'
+    return x*x
+# 查看方法文档(这里要去掉后面的括号)：方法名._doc_
+print(square.__doc__)
+y = square(2)
+print(y)
 
 # 练习
 def story(**kwds):
