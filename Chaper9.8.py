cbc8f9be33dfb9480bc53dd0c73237fc77ef131c
yangshunfan <15700734908@163.com>
2018-02-01 17:43:08 +0800

    八皇后问题

---
+++ Chaper9.8.py
@@ -0,0 +1,37 @@
+# 八皇后问题
+# 已知的皇后的位置被传递诶conflict函数，然后又函数判断下一个皇后的位置会不会又冲突
+def conflict(state, nextX):
+    # nextX表示水平位置（X坐标）nextY表示垂直位置（Y坐标）
+    nextY = len(state)
+    for i in range(nextY):
+        # 如果下一个皇后和正在考虑的前一个皇后的水平距离为0或等于垂直距离，就返回True
+        if abs(state[i] - nextX) in (0, nextY - 1):
+            return True
+    return False
+
+
+# 最后一个皇后时
+def queens(num=8, state=()):
+    for pos in range(num):
+        if not conflict(state, pos):
+            if len(state) == num - 1:
+            # yield类似返回return，但是下一次直接从yield下一行代码开始执行
+                yield (pos,)
+            else:
+                for result in queens(num, state + (pos,)):
+                    yield (pos,) + result
+
+
+# 最后处理
+def prettyprint(solution):
+    # 函数内部的函数为助手函数
+    def line(pos, length=len(solution)):
+        return '. ' * pos + '* ' + '. ' * (length - pos - 1)
+
+    for pos in solution:
+        print(line(pos))
+
+
+print(len(list(queens(8))))
+
+prettyprint(list(queens(8))[1])
